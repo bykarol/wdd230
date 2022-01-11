efdf35f6a31b061b040317d16c1c1bf6c68cc781
*brachokarol* <*brachokarol.sud@gmail.com*>
2022-01-11 16:59:50 +0100

    Assignment portal page w01 DONE!

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,3 +1,15 @@
-// toLocaleDateString
+function date() {
+  /*Actualizando el año actual en el footer*/
+  document.getElementById("current-year").innerHTML = new Date().getFullYear();
+
+  /*Fecha de última visita*/
+  const now = new Date(document.lastModified);
+  const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+  document.getElementById("date").innerHTML = fulldate;
+}
+/*Llamada a la función date()*/
+date();
+
+/* // toLocaleDateString
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById("currentdate").textContent = new Date().toLocaleDateString('en-US', options);
+document.getElementById("currentdate").textContent = new Date().toLocaleDateString('en-US', options); */
