2df2eafc9fbb86f8de86c95e929ab5ddbab11071
*bykarol* <*brachokarol.sud@gmail.com*>
2022-02-19 14:25:51 +0100

    DAYS VISITED DONE

---
+++ chamber/js/visits.js
@@ -0,0 +1,23 @@
+// initialize display elements
+const visitsDisplay = document.getElementById("visits");
+
+document.addEventListener("DOMContentLoaded", () =>{
+	populateStorage();
+});
+
+const populateStorage = () => {
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+};
