f16d89e33eae41432658bef4ac5acd1769593b2f
*brachokarol* <*brachokarol.sud@gmail.com*>
2022-02-25 12:28:21 +0100

    getting date from input hidden

---
+++ chamber/js/formDate.js
@@ -0,0 +1,7 @@
+/* --------------CURRENT DATA --------------------*/
+function formDate() {
+  /*Actualizando el año actual en el footer*/
+  document.getElementById("dateForm").nodeValue = new Date();
+}
+/*Llamada a la función formDate()*/
+formDate();
