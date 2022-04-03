2f2ace4028a391747fa46da7a4bcc5369ebb6c50
bykarol <brachokarol.sud@gmail.com>
2022-04-03 21:43:47 +0200

    nav menu & menu nested DONE

--- temple-project/js/temples.js
+++ temple-project/js/temples.js
@@ -13,16 +13,21 @@ fetch(requestURL)
       let h2 = document.createElement('h2');
       let p1 = document.createElement('p');
       let p2 = document.createElement('p');
-      let image = document.createElement('img');  
+      let image = document.createElement('img');
       image.setAttribute('src', temple[i].image);
       image.setAttribute('alt', `image of ${temple[i].name}`);
       h2.textContent = `${temple[i].name}`;
       p1.textContent = `${temple[i].address}`;
-      p2.textContent = `${temple[i].phone}`;  
+      p2.textContent = `${temple[i].phone}`;
+      let likeBtn = document.createElement('button');
+      likeBtn.name = "favTemple";
+      likeBtn.className = "likeBtn";
+      likeBtn.textContent = "Like";
       card.appendChild(h2);
       card.appendChild(image);
       card.appendChild(p1);
       card.appendChild(p2);
+      card.appendChild(likeBtn);
       document.querySelector('div.cards').appendChild(card);
     } 
 
